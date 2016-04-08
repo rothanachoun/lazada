274426c6a71fc63652a4926a78c7412a628ec50c
Rothana Choun <rothana.choun7@gmail.com>
2016-04-08 10:53:16 +0700

    get categories

--- lib/lazada/client.rb
+++ lib/lazada/client.rb
@@ -74,5 +74,19 @@ module Lazada
       response
     end
 
+    def get_categories
+      url = request_url('GetCategoryTree')
+      response = self.class.get(url)
+
+      response['SuccessResponse']['Body']['Categories']['Category']
+    end
+
+    def get_category_attributes(primary_cateogory_id)
+      url = request_url('GetCategoryAttributes', 'PrimaryCategory' => primary_cateogory_id)
+      response = self.class.get(url)
+
+      response['SuccessResponse']['Body']['Attribute']
+    end
+
   end
 end
