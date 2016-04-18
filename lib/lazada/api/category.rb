8c64b99d427c6eb0e32191dc5c0dc7ead509890e
Rothana Choun <rothana.choun7@gmail.com>
2016-04-18 08:47:04 +0700

    handle error request

--- lib/lazada/api/category.rb
+++ lib/lazada/api/category.rb
@@ -5,14 +5,16 @@ module Lazada
         url = request_url('GetCategoryTree')
         response = self.class.get(url)
 
-        response['SuccessResponse']['Body']['Categories']['Category']
+        return response['SuccessResponse']['Body']['Categories']['Category'] if response['SuccessResponse']
+        response
       end
 
       def get_category_attributes(primary_cateogory_id)
         url = request_url('GetCategoryAttributes', 'PrimaryCategory' => primary_cateogory_id)
         response = self.class.get(url)
 
-        response['SuccessResponse']['Body']['Attribute']
+        return response['SuccessResponse']['Body']['Attribute'] if response['SuccessResponse']
+        response
       end
     end
   end
